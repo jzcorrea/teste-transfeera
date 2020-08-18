20a4069fc0b14b7759a99206a97ce6703c0f79d7
José Leonardo Correa <jzcorrea23@gmail.com>
2020-08-17 22:32:26 -0300

    front-end: table

--- api/app.js
+++ api/app.js
@@ -1,10 +1,23 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
+    app.use(cors());
+
+    next();
+});
+
 app.use(require('./src/common/routes'));
 
 module.exports = app;
